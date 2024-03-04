0e7d4f1eb50d57e1465528eea5911ee8193c26a8
PauloZupper <paulo.fernandes@zup.com.br>
2024-03-04 10:48:07 -0300

    Updates

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Button'
-export * from './Tooltip'
-export * from './Input'
--- src/utils/getComponentPosition.ts
+++
@@ -1,4 +0,0 @@
-export const getComponentPosition = (ref?: DOMRect) => {
-  if(ref && ref.top > 400) return 'top'
-  return 'bottom'
-}
